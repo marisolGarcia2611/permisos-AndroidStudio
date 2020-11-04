299aca10948f22fbb3e24772490af6cc7968c2c0
Marisol <19170157@uttcampus.edu.mx>
2020-11-03 18:38:39 -0600

    agrege para que me verfifique si ya tengo los permisos

--- app/src/main/java/com/marisolgarcia2611/permisos/MainActivity.java
+++ app/src/main/java/com/marisolgarcia2611/permisos/MainActivity.java
@@ -1,14 +1,44 @@
 package com.marisolgarcia2611.permisos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn1);
+
+        int p1= ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+        int p2=ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION);
+        if(p1== PackageManager.PERMISSION_DENIED||p2==PackageManager.PERMISSION_DENIED)
+        {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE,Manifest.permission.ACCESS_FINE_LOCATION});
+                return;
+            }
+        }
+        hacerLlamada();
+    }
+
+    private void hacerLlamada() {
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel"+"8718877289")));
+    }
+
+    private void requestPermissions(String[] strings) {
     }
 }
